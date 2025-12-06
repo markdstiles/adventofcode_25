aff96363ee880fdc03b1140507c87d45fe28fa17
markdstiles <mark.d.stiles@gmail.com>
2025-12-06 22:28:55 +0000

    Day 1 part 2

--- src/day_1.rs
+++ src/day_1.rs
@@ -17,17 +17,41 @@ impl Safe {
         }
     }
 
-    fn rotate_dial(&mut self, rotation: i32) {
+    fn rotate_dial(&mut self, rotation: i32) -> i32 {
+        if rotation == 0 {
+            return 0
+        }
+
+        let dial_started_at_0 = self.dial == 0;
         self.dial += rotation;
 
-        while self.dial < self.dial_min || self.dial > self.dial_max {
-            if self.dial < self.dial_min {
+        if self.dial == 0 {
+            return 1
+        }
+
+        let mut zero_count = 0; // counter for the number of times we pass 0
+
+        // for positive values we can simply use division and the remainder to determine the dial position and times we passed 0
+        if self.dial > self.dial_max {
+            zero_count = self.dial / (self.dial_max+1);
+            self.dial %= self.dial_max+1;
+        }
+
+        // for negative values we'll simulate turning the dial
+        if self.dial < self.dial_min {
+            zero_count = if dial_started_at_0 { 0 } else { 1 }; // we have already passed 0 once unless the dial started on 0
+
+            while self.dial < self.dial_min {
                 self.dial += self.dial_max+1;
-            }
-            if self.dial > self.dial_max {
-                self.dial -= self.dial_max+1;
+
+                if self.dial <= self.dial_min {
+                    // if the dial is still 0 or negative we hit 0
+                    zero_count += 1;
+                }
             }
         }
+
+        zero_count
     }
 
     fn get_dial_position(&self) -> i32 {
@@ -44,6 +68,16 @@ fn apply_rotations(safe: &mut Safe, rotations: &[i32]) -> i32 {
         if safe.get_dial_position() == 0 {
             result += 1;
         }
+    }
+
+    result
+}
+
+fn apply_rotations_part2(safe: &mut Safe, rotations: &[i32]) -> i32 {
+    let mut result = 0;
+
+    for rotation in rotations {
+        result += safe.rotate_dial(*rotation);
     }
 
     result
@@ -84,9 +118,19 @@ pub fn do_part2() -> anyhow::Result<i64> {
 
     let file = File::open(input_file)?;
     let reader = BufReader::new(file);
-    
+    let mut rotations: Vec<i32> = Vec::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        let line = line.replace("L", "-");
+        let line = line.replace("R", "+");
+        rotations.push(line.parse()?);
+    }
+
+    let mut safe = Safe::new();
+    let zeros = apply_rotations_part2(&mut safe, &rotations) as i64;
    
-    Ok(0)
+    Ok(zeros)
 }
 
 #[test]
@@ -98,43 +142,100 @@ fn dial_starts_at_50(){
 #[test]
 fn move_dial_forward() {
     let mut safe = Safe::new();
-    safe.rotate_dial(14);
+    let zero_count = safe.rotate_dial(14);
     assert_eq!(safe.get_dial_position(), 64);
+    assert_eq!(zero_count, 0);
 }
 
 #[test]
 fn move_dial_backward() {
     let mut safe = Safe::new();
-    safe.rotate_dial(-14);
+    let zero_count = safe.rotate_dial(-14);
     assert_eq!(safe.get_dial_position(), 36);
+    assert_eq!(zero_count, 0);
 }
 
 #[test]
 fn move_dial_forward_past_99() {
     let mut safe = Safe::new();
-    safe.rotate_dial(54);
+    let zero_count = safe.rotate_dial(54);
     assert_eq!(safe.get_dial_position(), 4);
+    assert_eq!(zero_count, 1);
 }
 
 #[test]
 fn move_dial_backward_past_0() {
     let mut safe = Safe::new();
-    safe.rotate_dial(-54);
+    let zero_count = safe.rotate_dial(-54);
     assert_eq!(safe.get_dial_position(), 96);
+    assert_eq!(zero_count, 1);
 }
 
 #[test]
 fn rotate_dial_back_to_current_position_forward() {
     let mut safe = Safe::new();
-    safe.rotate_dial(100);
+    let zero_count = safe.rotate_dial(100);
     assert_eq!(safe.get_dial_position(), 50);
+    assert_eq!(zero_count, 1);
 }
 
- #[test]
+#[test]
 fn rotate_dial_back_to_current_position_backward() {
-let mut safe = Safe::new();
-    safe.rotate_dial(-100);
+    let mut safe = Safe::new();
+    let zero_count = safe.rotate_dial(-100);
     assert_eq!(safe.get_dial_position(), 50);
+    assert_eq!(zero_count, 1);
+}
+
+#[test]
+fn rotate_dial_back_to_current_position_forward_1000() {
+    let mut safe = Safe::new();
+    let zero_count = safe.rotate_dial(1000);
+    assert_eq!(safe.get_dial_position(), 50);
+    assert_eq!(zero_count, 10);
+}
+
+#[test]
+fn rotate_dial_to_0_forward_150() {
+    let mut safe = Safe::new();
+    let zero_count = safe.rotate_dial(150);
+    assert_eq!(safe.get_dial_position(), 0);
+    assert_eq!(zero_count, 2);
+}
+
+#[test]
+fn rotate_dial_to_0_backward_150() {
+    let mut safe = Safe::new();
+    let zero_count = safe.rotate_dial(-150);
+    assert_eq!(safe.get_dial_position(), 0);
+    assert_eq!(zero_count, 2);
+}
+
+#[test]
+fn rotate_dial_to_0_backward_250() {
+    let mut safe = Safe::new();
+    let zero_count = safe.rotate_dial(-250);
+    assert_eq!(safe.get_dial_position(), 0);
+    assert_eq!(zero_count, 3);
+}
+
+#[test]
+fn rotate_dial_back_to_current_position_backward_1050() {
+    let mut safe = Safe::new();
+    let zero_count = safe.rotate_dial(-1050);
+    assert_eq!(safe.get_dial_position(), 0);
+    assert_eq!(zero_count, 11);
+}
+
+#[test]
+fn rotate_dial_to_zero_then_to_0_again_backward() {
+    let mut safe = Safe::new();
+    let zero_count = safe.rotate_dial(-50);
+    assert_eq!(safe.get_dial_position(), 0);
+    assert_eq!(zero_count, 1, "move to 0 from 50");
+    let zero_count = safe.rotate_dial(-100);
+    assert_eq!(safe.get_dial_position(), 0);
+    assert_eq!(zero_count, 1, "move to 0 from 0 (left)");
 }
 
 #[test]
@@ -143,4 +244,12 @@ fn apply_test_rotations() {
     let mut safe = Safe::new();
     let result = apply_rotations(&mut safe, &rotations);
     assert_eq!(result, 3);
+}
+
+#[test]
+fn apply_test_rotations_part2() {
+    let rotations = vec![-68, -30, 48, -5, 60, -55, -1, -99, 14, -82];
+    let mut safe = Safe::new();
+    let result = apply_rotations_part2(&mut safe, &rotations);
+    assert_eq!(result, 6);
 }
