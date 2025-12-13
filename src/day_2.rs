c2ee29959775619147525da118bc1a4a8e795383
Mark Stiles <mark.d.stiles@gmail.com>
2025-12-13 23:48:30 +0000

    Day 2 solution - using brute force

--- src/day_2.rs
+++ src/day_2.rs
@@ -1,6 +1,6 @@
 //https://adventofcode.com/2025/day/2
 
-use std::{fs::File, io::{BufRead, BufReader, Read}};
+use std::{cmp::max, fs::File, io::{BufRead, BufReader, Read}};
 
 pub fn do_part1() -> anyhow::Result<i64> {
     println!("Day 2:");
@@ -44,8 +44,114 @@ pub fn do_part2() -> anyhow::Result<i64> {
     let file = File::open(input_file)?;
     let reader = BufReader::new(file);
 
-   
-    Ok(0)
+    let mut ranges:Vec<(usize, usize)> = Vec::new();
+
+    if let Some(line) = reader.lines().take(1).next() {
+        let line = line?;
+        ranges = line.split(",")
+        .map(|range_str| 
+            range_str.split_once("-")
+            .map_or((0, 0), |s| (s.0.parse().unwrap(), s.1.parse().unwrap()))).collect();
+    }
+
+    let mut total: usize = 0;
+    for range in ranges {
+        let range = InvalidIdRange::new(range.0, range.1);
+        let sum: usize = range.sum();
+        total += sum;
+    }
+
+    Ok(total as i64)
+}
+
+// Part 2 - Strategy: Brute-force - test each number in the range to see if its invalid!
+fn is_invalid_id(number: usize) -> bool {
+    // From scanning the ranges, the longest range number is 11 digits
+    // therefore for even numbers we only need to divide the number up 
+    // in a max of 5 segments (10) for comparison
+    // Invalid Ids for all odd numbers apart from 9 will be all the same digit
+    // For 9 we'll need to split it into 3 segments and compare
+    let len = get_num_digits(number);
+
+    if len == 1 {
+        return false
+    }
+    
+    if len.is_multiple_of(2) {
+        // Even number
+        if len.is_multiple_of(5) {
+            // Can be split into fifths? (len 10)
+            //Take first 2 digits and make invalid id
+            let segment = number / POW10[len-2];
+            let invalid_id: usize = segment.to_string().repeat(len/2).parse().unwrap();
+            if number == invalid_id {
+                return true
+            }
+        }
+        if len.is_multiple_of(4) {
+            // Can be split into quarters? (len 8)
+            //Take first 2 digits and make invalid id
+            let segment = number / POW10[len-2];
+            let invalid_id: usize = segment.to_string().repeat(len/2).parse().unwrap();
+            if number == invalid_id {
+                return true
+            }
+        }
+        if len.is_multiple_of(3) {
+            // Can be split into thirds? (len 6)
+            //Take first 2 digits and make invalid id
+            let segment = number / POW10[len-2];
+            let invalid_id: usize = segment.to_string().repeat(len/2).parse().unwrap();
+            if number == invalid_id {
+                return true
+            }
+        }
+        // Split in half
+        let invalid_id = generate_invalid_id(get_first_half(number));
+        number == invalid_id
+    } else {
+        // Odd number
+        if len == 9 {
+            //Take first 3 digits and make invalid id
+            let segment = number / 1_000_000;
+            let invalid_id: usize = segment.to_string().repeat(3).parse().unwrap();
+            number == invalid_id
+        } else {
+            //Take first digit and make invalid id
+            let first = number / POW10[len-1];
+            let invalid_id: usize = first.to_string().repeat(len).parse().unwrap();
+            number == invalid_id
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct InvalidIdRange {
+    current: usize,
+    end: usize,
+}
+
+impl InvalidIdRange {
+    fn new(from: usize, to: usize) -> Self {
+        InvalidIdRange { current: from, end: to }
+    }
+}
+
+impl Iterator for InvalidIdRange {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.current <= self.end {
+            let val = self.current;
+            self.current += 1;
+
+            if is_invalid_id(val) {
+                return Some(val)
+            }
+        }
+
+        None
+    }   
 }
 
 // lookup table for powers of 10
@@ -243,4 +349,73 @@ fn day1_proof() {
         total += sum;
     }
     assert_eq!(total, 1227775554);
+}
+
+// Part 2 Tests
+#[test]
+fn empty_range() {
+    let mut range = InvalidIdRange::new(1, 10);
+    assert_eq!(range.next(), None);
+}
+
+#[test]
+fn range_with_one_invalid_id() {
+    let mut range = InvalidIdRange::new(10, 21);
+    assert_eq!(range.next(), Some(11));
+    assert_eq!(range.next(), None);
+}
+
+#[test]
+fn range_with_two_invalid_ids() {
+    let mut range = InvalidIdRange::new(11, 22);
+    assert_eq!(range.next(), Some(11));
+    assert_eq!(range.next(), Some(22));
+    assert_eq!(range.next(), None);
+}
+
+#[test]
+fn range_with_triple_digits() {
+    let mut range = InvalidIdRange::new(98, 115);
+    assert_eq!(range.next(), Some(99));
+    assert_eq!(range.next(), Some(111));
+    assert_eq!(range.next(), None);
+}
+
+#[test]
+fn day2_proof() {
+    let ranges = [
+        (11,22),
+        (95,115),
+        (998,1012),
+        (1188511880,1188511890),
+        (222220,222224),
+        (1698522,1698528),
+        (446443,446449),
+        (38593856,38593862),
+        (565653,565659),
+        (824824821,824824827),
+        (2121212118,2121212124)
+    ];
+    let expected = [
+        11 + 22,
+        99 + 111,
+        999 + 1010,
+        1188511885,
+        222222,
+        0,
+        446446,
+        38593859,
+        565656,
+        824824824,
+        2121212121
+    ];
+
+    let mut total: usize = 0;
+    for (i, range) in ranges.iter().enumerate() {
+        let range = InvalidIdRange::new(range.0, range.1);
+        let sum: usize = range.sum();
+        assert_eq!(expected[i], sum);
+        total += sum;
+    }
+    assert_eq!(total, 4174379265);
 }
