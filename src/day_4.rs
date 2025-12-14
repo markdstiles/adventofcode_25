15e8aa83b09bc953e0c0bec21d00e8f868bbee52
markdstiles <mark.d.stiles@gmail.com>
2025-12-14 14:23:11 +0000

    Day 4 wip

--- src/day_4.rs
+++ src/day_4.rs
@@ -13,9 +13,29 @@ pub fn do_part1() -> anyhow::Result<i64> {
     let file = File::open(input_file)?;
     let reader = BufReader::new(file);
 
+    let mut map: Vec<Vec<char>> = reader.lines()
+        .map(|line| 
+            line.unwrap()
+                .chars()
+                .collect::<Vec<char>>()
+            )
+        .collect();
     
+    //Process the map looking for forklift locations
+    mark_forklift_locations(&mut map);
 
-    Ok(0 as i64)
+    //Count forklift locations
+    let total = count_forklift_locations(&map);
+
+    Ok(total as i64)
+}
+
+fn mark_forklift_locations(map: &mut Vec<Vec<char>>) {
+
+}
+
+fn count_forklift_locations(map: &[Vec<char>]) -> usize {
+    map.iter().flatten().filter(|c| **c == 'x').count()
 }
 
 pub fn do_part2() -> anyhow::Result<i64> {
@@ -31,4 +51,43 @@ pub fn do_part2() -> anyhow::Result<i64> {
 
 
     Ok(0 as i64)
+}
+
+#[test]
+fn day1_proof() {
+    let input = [
+        "..@@.@@@@.",
+        "@@@.@.@.@@",
+        "@@@@@.@.@@",
+        "@.@@@@..@.",
+        "@@.@@@@.@@",
+        ".@@@@@@@.@",
+        ".@.@.@.@@@",
+        "@.@@@.@@@@",
+        ".@@@@@@@@.",
+        "@.@.@@@.@.",
+    ];
+
+    let mut map: Vec<Vec<char>> = input.map(|line| line.chars().collect::<Vec<char>>()).to_vec();
+
+    mark_forklift_locations(&mut map);
+    let total = count_forklift_locations(&map);
+
+    assert_eq!(map[0].iter().collect::<String>(), "..xx.xx@x.");
+    assert_eq!(map[1].iter().collect::<String>(), "x@@.@.@.@@");
+    assert_eq!(map[2].iter().collect::<String>(), "@@@@@.x.@@");
+    assert_eq!(map[3].iter().collect::<String>(), "@.@@@@..@.");
+    assert_eq!(map[4].iter().collect::<String>(), "x@.@@@@.@x");
+    assert_eq!(map[5].iter().collect::<String>(), ".@@@@@@@.@");
+    assert_eq!(map[6].iter().collect::<String>(), ".@.@.@.@@@");
+    assert_eq!(map[7].iter().collect::<String>(), "x.@@@.@@@@");
+    assert_eq!(map[8].iter().collect::<String>(), ".@@@@@@@@.");
+    assert_eq!(map[9].iter().collect::<String>(), "x.x.@@@.x.");
+    
+    assert_eq!(total, 13);
+}
+
+#[test]
+fn day2_proof() {
+
 }
--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,11 @@ use colored::Colorize;
 
 fn main() -> anyhow::Result<()> {
     
-    if let Ok(answer) = day_3::do_part1() {
+    if let Ok(answer) = day_4::do_part1() {
         println!("{} {}", "Part 1 answer:".green(), answer.to_string().blue().bold());
     }
 
-    if let Ok(answer) = day_3::do_part2() {
+    if let Ok(answer) = day_4::do_part2() {
         println!("{} {}", "Part 2 answer:".green(), answer.to_string().blue().bold());
     }
 
