3eaa502ab3906f7338245bad682649a2a5b2cc6b
markdstiles <mark.d.stiles@gmail.com>
2025-12-15 23:34:54 +0000

    Day 7 part 1 solution

--- src/day_7.rs
+++ src/day_7.rs
@@ -13,7 +13,35 @@ pub fn do_part1() -> anyhow::Result<i64> {
     let file = File::open(input_file)?;
     let reader = BufReader::new(file);
 
-    Ok(0 as i64)
+    let mut map: Vec<Vec<char>> = reader.lines().map(|line| line.unwrap().chars().collect()).collect();
+
+    if let Some(start) = map[0].iter().position(|c| *c == 'S') {
+        map[0][start] = '|';
+    }
+
+    let mut split_count: usize = 0;
+    let map_height = map.len();
+    let map_width = map[0].len();
+
+    for y in 1..map_height {
+        for x in 0..map_width {
+            if map[y-1][x] == '|' {
+                if map[y][x] == '^' {
+                    split_count += 1;
+                    if x != 0 {
+                        map[y][x-1] = '|';
+                    }
+                    if x != map_width - 1 {
+                        map[y][x+1] = '|';
+                    }
+                } else {
+                    map[y][x] = '|';
+                }
+            }
+        }
+    }
+
+    Ok(split_count as i64)
 }
 
 pub fn do_part2() -> anyhow::Result<i64> {
@@ -28,4 +56,55 @@ pub fn do_part2() -> anyhow::Result<i64> {
     let reader = BufReader::new(file);
 
     Ok(0 as i64)
+}
+
+#[test]
+fn day1_proof() {
+    let mut input = [
+        ".......S.......",
+        "...............",
+        ".......^.......",
+        "...............",
+        "......^.^......",
+        "...............",
+        ".....^.^.^.....",
+        "...............",
+        "....^.^...^....",
+        "...............",
+        "...^.^...^.^...",
+        "...............",
+        "..^...^.....^..",
+        "...............",
+        ".^.^.^.^.^...^.",
+        "...............",
+   ];
+
+   let mut map: Vec<Vec<char>> = input.map(|line| line.chars().collect()).to_vec();
+   if let Some(start) = map[0].iter().position(|c| *c == 'S') {
+    map[0][start] = '|';
+   }
+
+   let mut split_count: usize = 0;
+   let map_height = map.len();
+   let map_width = map[0].len();
+
+   for y in 1..map_height {
+    for x in 0..map_width {
+        if map[y-1][x] == '|' {
+            if map[y][x] == '^' {
+                split_count += 1;
+                if x != 0 {
+                    map[y][x-1] = '|';
+                }
+                if x != map_width - 1 {
+                    map[y][x+1] = '|';
+                }
+            } else {
+                map[y][x] = '|';
+            }
+        }
+    }
+   }
+
+   assert_eq!(split_count, 21);
 }
