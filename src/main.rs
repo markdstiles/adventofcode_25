fdb69ab4861699c7dbe6c04cfba41190a6c1ce90
Mark Stiles <mark.d.stiles@gmail.com>
2025-12-17 17:28:58 +0000

    Day 8 prep

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-pub mod day_7;
+pub mod day_8;
 
 use colored::Colorize;
 
 fn main() -> anyhow::Result<()> {
     
-    if let Ok(answer) = day_7::do_part1() {
+    if let Ok(answer) = day_8::do_part1() {
         println!("{} {}", "Part 1 answer:".green(), answer.to_string().blue().bold());
     }
 
-    if let Ok(answer) = day_7::do_part2() {
+    if let Ok(answer) = day_8::do_part2() {
         println!("{} {}", "Part 2 answer:".green(), answer.to_string().blue().bold());
     }
 
